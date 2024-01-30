747fd64fad2b7a9a26eb9a3af7b0346e0644ed3c
nihat417 <akreminihat@gmail.com>
2024-01-30 22:43:57 +0400

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='loginMain'>
+        <div className='loginTitle'>
+            <h1>Hello Again!</h1>
+            <h3>Welcome Back</h3>
+        </div>
+        <div className='loginInputs'>
+            <input type="email" placeholder='Email Address' />
+            <input type="password" placeholder='Password' />
+        </div>
+
+        <button></button>
+        <p></p>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+const store = configureStore({
+    reducer: {
+      posts: postsReducer,
+      comments: commentsReducer
+    }
+  })
